98df7adb81a63bbbefe8c0face721b27a523eadf
Max Louis <40130@longroad.ac.uk>
2015-01-07 13:16:14 +0000

    Table Generator

--- K-D.py
+++ K-D.py
@@ -1,20 +1,22 @@
-#Max Louis and Ferhat
-#XxOPT1Cz_C0DxX MLG!!! NOOOBS!!!
-#06/01/2015
-full_list = [
-    ["K1llmAchine",51,49],
-    ["bob7424",5,99],
-    ["hAxOr12",72,30],
-]
+#Max Louis
+#Table Generator v0.1
+#07/01/2015
+def inputs():
+    columns = int(input("How many columns?"))
+    column_list = []
+    for count in range(columns):
+        column_names = input("Please enter the column names.")
+        column_list.append(column_names)
+    return column_list
 
-row = print("|")
-align = ("{0:<15}{1:^1}{2:<8}{3:^1}{4:<5}")
-print(align.format("NAME",row,"KILLS",row,"DEATHS"))
-print("_"*50)
+def columns(column_list,width):
+    select = -1
+    for each in column_list:
+        select += 1
+        print("{0:<{width}}".format(column_list[select],width = width[select]),end="")
 
-for player in full_list:
-    print(align.format(player[0],row,player[1],row,player[2]))
-
-
-
-
+def width()
+    
+#MAIN
+column_list = inputs()
+columns(column_list)
